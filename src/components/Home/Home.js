4fbede5a784378f31a1d50344c8e079e6956d64c
Adrienne Dao <adriennedao@gmail.com>
2020-05-21 11:33:18 -0400

    add searchbar com

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -77,7 +77,7 @@ class Home extends Component {
               title={this.state.heroImage.original_title}
               text={this.state.heroImage.overview}
               />
-              <SearchBar />
+              <SearchBar callback={this.searchItems} />
             </div>  : null }
                 <FourColGrid />
                 <Spinner />
